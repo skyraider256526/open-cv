b15546c416d498baa503c2ae1db5d09ba37ca780
skyraider <akashyadav@gmail.com>
2019-11-14 09:29:33 +0530

    Initial Commit

---
+++ object-detection/ball-detection.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+
+def nothing():
+    pass
+
+
+cv2.namedWindow('Tracking')
+cv2.createTrackbar('LH', 'Tracking', 0, 255, nothing)
+cv2.createTrackbar('LS', 'Tracking', 0, 255, nothing)
+cv2.createTrackbar('LV', 'Tracking', 0, 255, nothing)
+
+cv2.createTrackbar('UH', 'Tracking', 255, 255, nothing)
+cv2.createTrackbar('US', 'Tracking', 255, 255, nothing)
+cv2.createTrackbar('UV', 'Tracking', 255, 255, nothing)
+
+
+while True:
+    frame = cv2.resize(cv2.imread(
+        '/home/skyraider/Projects/Python/opencv/sample/balls.jpg'), (200, 240))
+
+    # Changing the frame to HSV color space
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Getting the trackbar value
+    l_h = cv2.getTrackbarPos('LH', 'Tracking')
+    l_s = cv2.getTrackbarPos('LS', 'Tracking')
+    l_v = cv2.getTrackbarPos('LV', 'Tracking')
+
+    u_h = cv2.getTrackbarPos('UH', 'Tracking')
+    u_s = cv2.getTrackbarPos('US', 'Tracking')
+    u_v = cv2.getTrackbarPos('UV', 'Tracking')
+
+    # Decalring the threshold for mask
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+
+    # Applying mask
+    mask = cv2.inRange(hsv, l_b, u_b)
+
+    # Logiacl AND to get the ROI
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+
+    key = cv2.waitKey(0)
+    print(key)
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
